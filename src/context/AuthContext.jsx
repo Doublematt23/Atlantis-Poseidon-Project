df50a673460d3e5ca7b4290b455953f0dc3900a5
simonshampoo <simonshm5@gmail.com>
2024-03-21 05:12:35 -0500

    add credit comment

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,19 +1,34 @@
-/*-------------------------------------------------------------------------*
-*---									
-*---        AuthContext.jsx						
-*---	   		
-*---	----	----	----	----	----	----	----	----	---*
-*---									
-*---        Version 1.0     2024 March 21st   Simon Shamoon 	  	
-*---		       	
-*-------------------------------------------------------------------------*/
 
 import React, { createContext, useContext, useState, useEffect } from "react";
 
+/**
+ * AuthContext provides authentication-related data and functions to its descendants.
+ * @version 1.0
+ * @author Simon Shamoon 100%
+ * 2024 March 21st
+ * @property {boolean} isLoggedIn - Indicates whether the user is logged in or not.
+ * @property {function} setIsLoggedIn - Function to update the isLoggedIn state.
+ */
+
+/**
+ * AuthContext is a React context that provides authentication-related data and functions.
+ * @type {React.Context<AuthContextValue>}
+ */
 const AuthContext = createContext();
 
+/**
+ * Custom hook to access the AuthContext.
+ * @returns {AuthContextValue} - The value of the AuthContext.
+ */
 export const useAuth = () => useContext(AuthContext);
 
+/**
+ * AuthProvider is a component that wraps its children with the AuthContext.Provider.
+ * It manages the isLoggedIn state based on the presence of a token in the localStorage.
+ * @param {Object} props - The component props.
+ * @param {React.ReactNode} props.children - The children components to be wrapped.
+ * @returns {React.ReactNode} - The wrapped children components.
+ */
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
